225e8b1346d8b2ae721db81a43b528d1e22e74a8
liuchengzhe <chengzhe.lcz@alibaba-inc.com>
2019-04-17 15:17:41 +0800

    init the project

---
+++ src/components/CanvasScreen/index.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+class CanvasScreen extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div {...this.props}>
+
+      </div>
+    )
+  }
+}
+
+export default CanvasScreen;
